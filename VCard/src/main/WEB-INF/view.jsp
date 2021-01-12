9b29d0bb23231266626d302f3a4981828f3b75a6
Mmati1996 <mat.duk.at@gmail.com>
2021-01-12 14:33:31 +0100

    jsp added

---
+++ VCard/src/main/WEB-INF/view.jsp
@@ -0,0 +1,18 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<html>
+<head>
+    <title>Table Data Addition</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+</div>
+</body>
+</html>
--- VCard/src/main/java/com/example/VCard/DataController.java
+++ VCard/src/main/java/com/example/VCard/DataController.java
@@ -4,10 +4,9 @@ import com.google.gson.Gson;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
@@ -16,8 +15,9 @@ import java.util.ArrayList;
 public class DataController {
 
     @RequestMapping(path = "/search")
-    public ResponseEntity<String> panoramaFirmSearcher(@RequestParam String name, HttpServletResponse response){
-        String url = "https://panoramafirm.pl/szukaj?k="+name+"&l";
+    public ModelAndView panoramaFirmSearcher(@RequestParam String name, @RequestParam String city,@RequestParam HttpServletResponse response){
+        String url = "https://panoramafirm.pl/szukaj?k="+name+"&l="+city;
+        StringBuilder stringBuilder = new StringBuilder();
 
         org.jsoup.nodes.Document doc = null;
 
@@ -42,6 +42,7 @@ public class DataController {
 
 
         toReturn = str.toString();
-        return new ResponseEntity<String>( toReturn, HttpStatus.OK);
+        return new ModelAndView("view","",toReturn);
+        //return new ResponseEntity<String>( toReturn, HttpStatus.OK);
     }
 }
